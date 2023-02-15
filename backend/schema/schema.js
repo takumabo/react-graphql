b538b8ce4abb72d8030db584d778d163f92aefd3
takumabo <kingcongyatuhashi.1043@gmail.com>
2023-02-15 07:13:13 +0000

    get all items with relation

--- backend/schema/schema.js
+++ backend/schema/schema.js
@@ -59,11 +59,23 @@ const RootQuery = new GraphQLObjectType({
                 return lodashModules.find(books, { id: args.id })
             }
         },
+        books: {
+            type: new GraphQLList(BookType),
+            resolve(parent, args) {
+                return books
+            }
+        },
         author: {
             type: AuthorType,
             args: { id: { type: GraphQLID } },
             resolve(parent, args) {
                 return lodashModules.find(authors, { id: args.id })
+            }
+        },
+        authors: {
+            type: new GraphQLList(AuthorType),
+            resolve(parent, args) {
+                return authors
             }
         }
     }
