b5a5cee0a96206aa5dd3f70dd2a496bef071c27b
takumabo <kingcongyatuhashi.1043@gmail.com>
2023-02-16 07:58:02 +0000

    fix

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,5 @@
 import './App.css';
-import { BookList } from './components/BookList';
+import BookList from './components/BookList';
 
 function App() {
   return (
--- frontend/src/components/BookList.tsx
+++ frontend/src/components/BookList.tsx
@@ -1,6 +1,6 @@
 import Box from "@mui/material/Box";
 
-export function BookList() {
+export default function BookList() {
     return (
         <Box>book name</Box>
     )
