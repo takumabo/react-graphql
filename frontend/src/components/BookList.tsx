90b515e2bcf39476fbbb042d63f1d995d9922647
takumabo <kingcongyatuhashi.1043@gmail.com>
2023-02-16 10:22:28 +0000

    reqeust query from frontend

--- frontend/src/components/BookList.tsx
+++ frontend/src/components/BookList.tsx
@@ -1,6 +1,30 @@
 import Box from "@mui/material/Box";
+import { gql, useQuery } from "@apollo/client";
+
+const GET_BOOKS = gql`
+  query GetBooks {
+    books {
+        name
+        id
+    }
+  }
+`;
+
+// const GET_AUTHORS = gql`
+//     query GetAuthors {
+//         authors {
+//             name
+//             age
+//         }
+//     }
+// `
 
 export default function BookList() {
+    const { loading, error, data } = useQuery(GET_BOOKS)
+    if (loading) return (<Box>loading</Box>);
+    if (error) return <Box>Error : {error.message}</Box>;
+
+    console.log({ data })
     return (
         <Box>book name</Box>
     )
