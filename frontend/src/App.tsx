f49907cab8d927228d83edfa5cb9f1bc91e9b38c
takumabo <kingcongyatuhashi.1043@gmail.com>
2023-02-16 08:44:54 +0000

    create client server

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,21 @@
 import './App.css';
 import BookList from './components/BookList';
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
 
+// call endpoint from .env 
+const uri = process.env.REACT_APP_BACKEND_ENDPOINT
+
+// setup apollo client
+const cache = new InMemoryCache();
+const apollo = new ApolloClient({
+  uri,
+  cache
+})
 function App() {
   return (
-    <>
+    <ApolloProvider client={apollo}>
       <BookList />
-    </>
+    </ApolloProvider>
   );
 }
 
